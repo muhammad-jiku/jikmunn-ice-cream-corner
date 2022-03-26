4049005856183363e2ff170dd2db5cbcdfbd4451
muhammad-jiku <muhammadjiku364@gmail.com>
2022-03-26 09:16:11 +0600

    creating fake data and fetching data

--- src/components/IceCreameShop/IceCreameShop.js
+++ src/components/IceCreameShop/IceCreameShop.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const IceCreameShop = () => {
+  const [iceCreams, setIceCreams] = useState([]);
+
+  useEffect(() => {
+    fetch('iceCreams.json')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h1>Lovable Ice Cream Shop</h1>
       <div className="iceCreamShopContainer">
         <h1>Ice Cream Shop</h1>
+        {iceCreams.map((iceCream) => console.log(iceCream))}
       </div>
       <div className="iceCreamCartContainer">
         <h1>Ice Cream Cart</h1>
