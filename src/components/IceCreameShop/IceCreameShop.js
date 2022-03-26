44f5125f54f5a4012e72dde5a17ff9a09c7f227e
muhammad-jiku <muhammadjiku364@gmail.com>
2022-03-26 08:40:50 +0600

    adding components

---
+++ src/components/IceCreameShop/IceCreameShop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const IceCreameShop = () => {
+  return (
+    <div>
+      <h1>Lovable Ice Cream Shop</h1>
+      <div className="iceCreamShopContainer">
+        <h1>Ice Cream Shop</h1>
+      </div>
+      <div className="iceCreamCartContainer">
+        <h1>Ice Cream Cart</h1>
+      </div>
+    </div>
+  );
+};
+
+export default IceCreameShop;
